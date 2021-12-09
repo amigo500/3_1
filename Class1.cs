16abe94a941c8df965d40c505489712b124bcb17
amigo500 <alexhouser@hotmail.com>
2021-12-09 01:24:24 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+        public class Person
+    {
+        private int age;
+        private string fname;
+        private string lname;
+        private int height;
+        private int weight;
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+        public string Fname
+        {
+            get => fname;
+            set => fname = value;
+        }
+        public string Lname
+        {
+            get => lname;
+            set => lname = value;
+        }
+        public int Height
+        {
+            get => height;
+            set => height = value;
+        }
+        public int Weight
+        {
+            get => weight;
+            set => weight = value;
+        }
+        public Person(int age, string fname, string lname, int height, int weight)
+        {
+            Age = age;
+            Fname = fname;
+            Lname = lname;
+            Height = height;
+            Weight = weight;
+                    }
+            }
+
+    
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+    public class Program
+    {
+                    static void Main(string[] args)
+        {
+            Person person1 = new Person(6, "Leopold", "Bloom", 178, 67);
+            Console.WriteLine("person1 Age = {0} Fname = {1}", person1.Age, person1.Fname);
+
+            // Declare new person, assign person1 to it.
+            Person person2 = person1;
+
+            // Change the name of person2, and person1 also changes.
+            person2.Fname = "Molly";
+            person2.Age = 16;
+        person2.Lname = "Sandén";
+        person2.Height = 166;
+        person2.Weight = 50;
+
+        Console.WriteLine("person2 Age = {0} Fname = {1}", person2.Age, person2.Fname);
+            Console.WriteLine("person1 Age = {0} Fname = {1}", person1.Age, person1.Fname);
+        }
+    }
